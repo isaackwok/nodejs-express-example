ef60e66fc6fee12c9448af0873734ed8db1c0689
janniefish <shibainu831@gmail.com>
2019-08-04 16:04:36 +0800

    我好棒

--- homework/hw1/06130336.js
+++ homework/hw1/06130336.js
@@ -1,7 +1,9 @@
 const exp = require('express')(); //let
+var path = require('path');
 
 exp.get('/',function(req,res){
-    res.send("我好棒");
+    // res.send("我好棒");
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 exp.listen(9487,function(){
