bd23a2e43a4a47c6b5a7fbe150207d4949c09f86
Pei-Ting-Wang <53286421+Pei-Ting-Wang@users.noreply.github.com>
2019-08-25 13:07:35 +0800

    hw_06130035
    ✔1.網頁內容不能與ex3一樣，內容自行構想
    ✔2.除了根目錄以外，至少要有2個分流(route)，都要用到query string(至少一個sendFile跟一個send)
    response裡除了基本的顯示資訊，請加上任意計算(例如計算BMI)
    X 3.請在每個分流都加入回到首頁的功能(未完成)

---
+++ homework/hw2/06130035/bmi.js
@@ -0,0 +1,25 @@
+const app=require('express')();
+let path=require('path');
+const port=3000;
+app.get('/',function(req,res){
+    console.log("Recieved GET request on [/] path.");
+    res.sendFile(path.join(__dirname, 'bmi.html')); 
+});
+app.get('/send',function(req,res){
+	res.send("<h1>這是我的send分頁:D</h1>");
+
+	
+});
+
+app.get('/show',function(req,res){
+	var height=req.query.height;
+	var weight=req.query.weight;
+	height /=100;
+	var bmi=weight/(height*height);
+	
+	res.send("<h1 style='color:red'>你的BMI值 :"+bmi+"</h1>");
+	
+});
+app.listen(port,function(){
+	 console.log("Listening on " + port + " port.");
+});
