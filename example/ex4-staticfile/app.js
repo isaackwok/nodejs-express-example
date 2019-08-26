6fa49459963a0c2db0e266c5cbfd3baec3b3ab56
Kwok Chun Ho <kch062522@gmail.com>
2019-08-26 15:57:40 +0800

    Updated ex4 AND adjust some code in ex3

--- example/ex4-staticfile/app.js
+++ example/ex4-staticfile/app.js
@@ -1,28 +1,45 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 let path = require('path');
-const port = 5500; //定義port
+const port = 5500; 
+
+//定義static目錄，以使用靜態檔案
+app.use(express.static('public'));
 
 //[GET] 根路徑
 app.get('/',function(req,res){
     console.log("Recieved GET request on [/] path.");
-    res.sendFile(path.join(__dirname, 'index.html')); //使用sendFile傳送html檔
+    res.sendFile(path.join(__dirname, 'index.html')); 
 });
 
 //[GET] 路徑: /show
 app.get('/show',function(req,res){
     console.log("Recieved GET request on [/show] path.");
 
-    //使用ex2的query方式取得資料(從index.html的form submit後得來)
     let name = "Name: " + req.query.name;
     let studentID = "Student ID: " + req.query.studentID;
     let email = "E-mail: " + req.query.email;
 
-    //處理輸出資料
     let br = '<br>';
     let information = name + br + studentID + br + email;
     let form = '<form method="GET" action="/"><input type="submit" value="回到首頁" /></form>';
-    //使用簡單的send回傳資料
+ 
     res.send("<h1>" + information + "</h1>" + form);
+});
+
+app.post('/test', function(req,res){
+    console.log("Recieved POST request on [/test] path.");
+    res.send("<h1>This is POST.</h1>");
+});
+
+app.delete('/test', function(req,res){
+    console.log("Recieved POST request on [/test] path.");
+    res.send("<h1>This is DELETE.</h1>");
+});
+
+app.put('/test', function(req,res){
+    console.log("Recieved POST request on [/test] path.");
+    res.send("<h1>This is TEST.</h1>");
 });
 
 app.listen(port,function(){
