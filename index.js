0c76cd151a6d389f7d1a52327c3f7698455e7ee6
Kwok Chun Ho <kch062522@gmail.com>
2019-08-26 19:36:50 +0800

    test

---
+++ index.js
@@ -0,0 +1,14 @@
+const app = require('express')(); 
+var http = require('http').createServer(app);
+
+app.get('/', function(req,res){
+    res.send('<h1>Hello！</h1>');
+});
+
+http.listen(process.env.PORT || 5000,function(){
+    console.log('listening on port 5000');
+});
+// app.listen(process.env.PORT || 3000, function(){
+
+//     console.log('Express網站正在監聽3000 port.');
+// });
