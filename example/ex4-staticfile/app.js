a1c9c104f47ddf652140d6f5c9e327ab82e3a889
Kwok Chun Ho <kch062522@gmail.com>
2019-08-26 19:27:51 +0800

    update project

--- example/ex4-staticfile/app.js
+++ example/ex4-staticfile/app.js
@@ -1,7 +1,7 @@
 const express = require('express')();
 const app = express();
 let path = require('path');
-const port = 5500; 
+const port = 3000; 
 
 //定義static目錄，以使用靜態檔案
 app.use(express.static('public'));
--- index.js
+++
@@ -1,9 +0,0 @@
-let app = require('express')();
-
-app.get('/',function(req,res){
-    res.send('Hello');
-});
-
-app.listen(process.env.PORT || 5000,function(){
-    console.log("Listening on " + port + " port.");
-});
