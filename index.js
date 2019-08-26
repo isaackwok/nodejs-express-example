9968eb7fecf25505666a2a826d0d28780500fdf4
Kwok Chun Ho <kch062522@gmail.com>
2019-08-26 18:56:02 +0800

    test

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 let app = require('express')();
 
-app.
+app.get('/',function(req,res){
+    res.send('Hello');
+});
+
+app.listen(process.env.PORT || 5000,function(){
+    console.log("Listening on " + port + " port.");
+});
